0aea7257d20fd29acc6233dfc050668339939b0d
jdzul123 <jdzul@devbot.com>
2021-12-13 11:11:03 -0800

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,8 +32,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
+             services.AddCors();
+             
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -44,6 +46,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 
             } );
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,10 +58,11 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
-
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
+             app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
